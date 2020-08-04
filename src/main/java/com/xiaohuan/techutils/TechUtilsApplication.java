a1edf715813302965af0c27c89cb763f21e9e2be
xiaofanTo <1032962558@qq.com>
2020-08-04 21:00:52 +0800

    init project

---
+++ src/main/java/com/xiaohuan/techutils/TechUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohuan.techutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaohuan/techutils/TechUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohuan.techutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
